c215c692457dbaeb5a9c80322d86053f5d4922b9
K-scorpi <mkirill602@gmail.com>
2025-04-22 23:08:27 +0500

    login

--- week_9/django_auth/django_auth/urls.py
+++ week_9/django_auth/django_auth/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("", include("users.urls")),
 ]
