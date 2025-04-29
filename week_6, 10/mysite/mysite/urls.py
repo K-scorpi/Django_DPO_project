d9ba57b2f270f11c32ce11545bf899cfdfbafd7c
K-scorpi <mkirill602@gmail.com>
2025-04-29 23:27:20 +0500

    import

--- week_6, 10/mysite/mysite/urls.py
+++ week_6, 10/mysite/mysite/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin 
 from django.urls import path, include 
 from blog import urls 
+from .import views 
  
 urlpatterns = [ 
     path('admin/', admin.site.urls), 
-    path('', include('blog.urls')), 
+    path('', include('blog.urls')),
 ] 
