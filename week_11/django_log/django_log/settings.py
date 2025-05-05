0c4a82e204fdaed812f7d05c5725d5137ee9aa0d
K-scorpi <mkirill602@gmail.com>
2025-05-06 00:53:43 +0500

    log site

--- week_11/django_log/django_log/settings.py
+++ week_11/django_log/django_log/settings.py
@@ -51,22 +51,35 @@ MIDDLEWARE = [
 ]
 
 LOGGING = {
-'version': 1,
-'disable_existing_loggers': False,
-'handlers': {
-    'file': {
-        'level': 'WARNING',
-        'class': 'logging.FileHandler',
-        'filename': BASE_DIR / 'warning.log',
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': 'warning.log',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file', 'console'],
+            'level': 'WARNING',
+            'propagate': True,
+        }
     }
-},
-'loggers': {
-    '': {
-        'handlers': ['file'],
-        'level': 'WARNING',
-        'propagate': True,
-    }
-}
 }
 
 ROOT_URLCONF = "django_log.urls"
